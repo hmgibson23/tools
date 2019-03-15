fcf76c5427d3587a71cbb097ad9a797071413c0e
Hugo Gibson <hugo.gibson@news.co.uk>
2019-03-15 12:23:39 +0000

    lololol

--- mail-checker/fetch-mail.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-PASS=$(gpg -qd ~/.mutt/password.gpg)
-OUT=$(./mail imaps://imap.gmail.com gibsonhugo@gmail.com $PASS) 
-
-echo $OUT | cut -d "(" -f2 | cut -d ")" -f1 | grep -o '.$'
-
