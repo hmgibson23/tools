324dcde697341adb83d610f2220cd713d0b8caad
Hugo Gibson <hugo.gibson@news.co.uk>
2019-03-22 12:36:41 +0000

    alpine dev r2g

---
+++ vms/alpine-devbox/setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+apk update
+apk add busybox-extras git openssh neovim vim \
+  mksh fish ca-certificates \
+  bash gcc musl-dev openssl \
+  go fzf bats tar wget stow make curl python3 tmux
+
+# this was lifted from the go dockerfile image
+[ ! -e /etc/nsswitch.conf ] && echo 'hosts: files dns' > /etc/nsswitch.conf
+export GOROOT_BOOTSTRAP="$(go env GOROOT)" \
+        GOOS="$(go env GOOS)" \
+        GOARCH="$(go env GOARCH)" \
+        GOHOSTOS="$(go env GOHOSTOS)" \
+        GOHOSTARCH="$(go env GOHOSTARCH)" \
+        GOLANG_VERSION="1.12"
+
+wget -O go.tgz "https://golang.org/dl/go$GOLANG_VERSION.src.tar.gz";
+tar -C /usr/local -xzf go.tgz;
+rm go.tgz;
+cd /usr/local/go/src;
+./make.bash;
+rm -rf /usr/local/go/pkg/bootstrap /usr/local/go/pkg/obj
+export PATH="/usr/local/go/bin:$PATH" >> /home/vagrant/.profile;
+
+# setup dotfiles
+git clone https://github.com/hmgibson23/snippets.git
+cd snippets && make stow
+
+# nvim plugins
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
