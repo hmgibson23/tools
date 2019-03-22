59ade5b8de1a412173348580bf5a3f8339b18959
Hugo Gibson <hugo.gibson@news.co.uk>
2019-03-22 15:39:54 +0000

    deletions

--- vms/alpine-devbox/setup.sh
+++ vms/alpine-devbox/setup.sh
@@ -23,9 +23,33 @@ cd /usr/local/go/src;
 rm -rf /usr/local/go/pkg/bootstrap /usr/local/go/pkg/obj
 export PATH="/usr/local/go/bin:$PATH" >> /home/vagrant/.profile;
 
+# terraform
+export TERRAFORM_VERSION=0.11.12
+export TF_DEV=true
+export TF_RELEASE=true
+export GOPATH="$HOME/go"
+mkdir -p "$GOPATH/src/github.com/hashicorp"
+cd "$GOPATH/src/github.com/hashicorp"
+git clone https://github.com/hashicorp/terraform.git && \
+git checkout v${TERRAFORM_VERSION} && \
+/bin/bash scripts/build.sh
+
+# kubectl
+cd $HOME
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ln -S /usr/bin/kubectl $HOME/kubectl
+
+
 # setup dotfiles
 git clone https://github.com/hmgibson23/snippets.git
 cd snippets && make stow
+
+# fasd
+wget https://github.com/clvv/fasd/tarball/1.0.1
+tar -xzf 1.0.1 && cd clvv-fasd-4822024 && make install
+
+# rust because I want ripgrep and rg
+curl https://sh.rustup.rs -sSf | sh
 
 # nvim plugins
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
